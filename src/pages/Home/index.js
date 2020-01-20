07c026e2976bd152862f4b91ad6ca3044ca51824
Matheus Santos <matheus_sa_santos@hotmail.com>
2020-01-20 09:19:54 -0300

    modulo07 completo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,10 +24,10 @@ class Home extends React.Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
   render() {
     const { products } = this.state;
@@ -42,7 +42,7 @@ class Home extends React.Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />{' '}
